410a41d26383f4e47bb4233fbe6e7783414db189
Kirill <litovchenko.br@gmail.com>
2023-04-13 19:44:49 +0300

    hw
    changed CSS

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -81,3 +81,10 @@ function updateClockField(time) {
   refs.minutesField.textContent = addLeadingZero(time.minutes);
   refs.secondsField.textContent = addLeadingZero(time.seconds);
 }
+
+refs.startBtn.style.cssText = ` 
+  background-color: red;
+  padding: 1rem;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+`;
